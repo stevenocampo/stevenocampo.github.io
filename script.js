c8785836918a4c53d964449f4c34d8166f692d8d
Steven Ocampo <so245@cornell.edu>
2020-09-18 01:34:41 -0400

    Use startUTC time to build the next Request to NASA SSC API

--- script.js
+++ script.js
@@ -57,8 +57,81 @@ $(document).ready(function() {
 			success: function(result) {
 				console.log("N2YO success");
 				console.log(result);
+				console.log(result.passes[0].startUTC);
+
+
+
+				// Now that the next visual pass has been requested. The next goal is to make a request to the SSC NASA API for coordinate data of the ISS over time to plot on the map
+
+				// Store the startUTC time of the next visual pass for the ISS depending on the selected coordinates
+				var startUnix = result.passes[0].startUTC;
+				console.log(startUnix);
+
+
+
+
+				// Context: I'll now be building the startTime & EndTime parameters for the SSC API Request, which will return the coordinate data of the ISS over the time range.
+
+				// Subtract 5 minutes from the startUTC time. NOTE: JS works in milliseconds so first I have to multiply by 1000ms and then subtract 5 minutes
+				startdateObj = new Date(startUnix * 1000 - 300000);
+				console.log(startdateObj);				
+
+				// Add 5 minutes to the startUTC time. NOTE: JS works in milliseconds so first I have to multiply by 1000ms and then add 5 minutes
+				enddateObj = new Date(startUnix * 1000 + 300000);
+				console.log(enddateObj);
+
+				
+
+				// Build the startTime paramaters for ISO 8601 format
+				var startGetYear = startdateObj.getFullYear();
+				var startGetMonth = startdateObj.getMonth() + 1;
+				var startGetDay = startdateObj.getDate();
+				var startGetHours = startdateObj.getHours();
+				var startGetMinutes = startdateObj.getMinutes();
+
+
+				// Build the endTime paramater for ISO 8601 format
+				var endGetYear = enddateObj.getFullYear();
+				var endGetMonth = enddateObj.getMonth() + 1;
+				var endGetDay = enddateObj.getDate();
+				var endGetHours = enddateObj.getHours();
+				var endGetMinutes = enddateObj.getMinutes();
+
+	
+
+
+				// Build startTime URL path. If the Month, Day, Hour, or Minute value is < 10, prepend a '0' to make the entire timestamp in ISO 8601 format
+				startUrlParam = "/" + startGetYear + startGetMonth.toString().padStart(2, '0') + startGetDay.toString().padStart(2, '0') + "T" + startGetHours.toString().padStart(2, '0') + startGetMinutes.toString().padStart(2, '0') + "00Z";
+				console.log(startUrlParam);
+
+				// Build endTime URL path. If the Month, Day, Hour, or Minute value is < 10, prepend a '0' to make the entire timestamp in ISO 8601 format
+				endUrlParam = "," + endGetYear + endGetMonth.toString().padStart(2, '0') + endGetDay.toString().padStart(2, '0') + "T" + endGetHours.toString().padStart(2, '0') + endGetMinutes.toString().padStart(2, '0') + "00Z";
+				console.log(endUrlParam);
+				
+
+
+
+				ajaxCall2();
+
 			}
-		});		
+		});
+
+
+		function ajaxCall2(){
+			$.ajax({
+				// url: "https://sscweb.gsfc.nasa.gov/WS/sscr/2/locations/iss/20200913T223000Z,20200913T224000Z/geo",
+				url: "https://sscweb.gsfc.nasa.gov/WS/sscr/2/locations/iss" + startUrlParam + endUrlParam + "/geo",
+				success: function(result2) {
+					
+					console.log("SSC API Success");
+					console.log(result2);
+
+				}
+
+			});			
+		}
+
+
 	});
 
 });
